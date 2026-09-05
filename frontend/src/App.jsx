30ae9ac1bf891fb1ff8ac40fd500e9116175cf50
rocklegend14 <gnaneshwarankumaravel12@gmail.com>
2026-09-05 20:09:15 +0530

    fixed app.jsx with import issue in naming navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { api } from './api';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar';
 import SummaryHero from './components/summaryHero';
 import RecoveryApprovalCard from './components/recoveryApprovalCard';
 import CauseBreakdown from './components/causeBreakdown';
